cc6ea8a7cbdbd962f36a9e4be078fdef3f5e3bce
OneDev0411 <cryptoapple1109@gmail.com>
2022-06-10 05:22:19 -0300

    #1 auto scrolling

--- app/components/form/steps/StartQuestion/index.tsx
+++ app/components/form/steps/StartQuestion/index.tsx
@@ -12,13 +12,13 @@ const StartQuestion: React.FC<IQuestionProps> = ({
     // mockup loading, need to remove after the backend is impplemented
     useEffect(() => {
         // wizard.next();
-        wizard.goto(10);
+        // wizard.goto(10);
         
-        // wizard.setLoading(true);
-        // setTimeout(() => {
-        //     wizard.setLoading(false);
-        //     wizard.next();
-        // }, 100);
+        wizard.setLoading(true);
+        setTimeout(() => {
+            wizard.setLoading(false);
+            wizard.next();
+        }, 100);
     }, []);
 
     return (
